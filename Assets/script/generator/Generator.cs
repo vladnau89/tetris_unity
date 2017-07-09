290db38ae21feb32a5045dd2c5f00ba3fd2c3452
vladnau89 <vladnau89@yandex.ru>
2017-07-09 15:52:07 +0300

    add generator scene. compare Unity and Dotnext Random and save result to textures /texture/random_DotNet.png and /texture/random_Unity.png

---
+++ Assets/script/generator/Generator.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Generator
+{
+    private delegate float GenerateRandomFunc();
+
+    private static System.Random r = new System.Random();
+
+    public enum Type
+    {
+        Unity,
+        DotNet,
+    }
+
+    public static float[] GenerateRandomArray01(int count, Type type)
+    {
+        GenerateRandomFunc func = null;
+
+        switch (type)
+        {   
+            case Type.Unity:
+                func = () => UnityEngine.Random.Range(0f, 1.0f);
+                break;
+            case Type.DotNet:
+                func = GetRandomDotnetFloat;
+                break;
+            default:
+                throw new System.Exception("undefined type = " + type);
+        }
+
+        return GenerateRandomArray(count, func);
+    }
+
+    private static float[] GenerateRandomArray(int count , GenerateRandomFunc func)
+    {
+        float[] nums = new float[count];
+
+        for (int i = 0; i < count; i++)
+        {
+            nums[i] = func();
+        }
+
+        return nums;
+    }
+
+    private static float GetRandomDotnetFloat()
+    {
+        int randomInt = r.Next(0, int.MaxValue);
+        return (float)randomInt / int.MaxValue;
+    }
+
+}
---
+++ Assets/script/generator/GeneratorManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class GeneratorManager : MonoBehaviour
+{
+    [SerializeField] private Generator.Type _type;
+    [SerializeField] private int _width = 1024;
+    [SerializeField] private int _height = 1024;
+
+    [ContextMenu("Generate")]
+    void Generate()
+    {
+        
+        Texture2D texture = new Texture2D(_width, _height, TextureFormat.Alpha8, false);
+        for (int i = 0; i < _height; i++)
+        {
+            float[] randArray = Generator.GenerateRandomArray01(_width, _type);
+
+            for (int y = 0;  y < _width;  y++)
+            {
+                texture.SetPixel(i, y, new Color(0, 0, 0, randArray[y]));
+            }
+
+        }
+
+        texture.Apply();
+
+        var bytes = texture.EncodeToPNG();
+
+        string path = string.Format("{0}/random_{1}.png", Path.Combine(Application.dataPath, "texture"), _type);
+
+        File.WriteAllBytes(path, bytes);
+
+
+        Debug.LogError("OK!");
+
+    }
+
+    //void OnValidate()
+    //{
+    //    string path = string.Format("{0}/random_{1}.png", Path.Combine(Application.dataPath, "textures"), _type);
+    //    Debug.LogError(path);
+    //}
+
+}
