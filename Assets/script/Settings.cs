91dce6081bdf939afd0880d3876e7a986043efd8
vladnau89 <vladnau89@yandex.ru>
2017-07-09 22:03:24 +0300

    add Settings

---
+++ Assets/script/Settings.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settings : MonoBehaviour
+{
+    private static Settings instance;
+
+    [SerializeField] private bool _wallKick = false;
+
+    public static bool WallKick { get { return instance._wallKick; } }
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+}
